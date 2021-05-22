4b8d29140bc682425521a4ac0a24bcb253c5b79c
Joshua Saps <joshuasapz@gmail.com>
2021-05-22 11:45:48 +0800

    added enter title function

--- Project.py
+++ Project.py
@@ -4,20 +4,27 @@ from nltk.tokenize import word_tokenize
 from nltk.probability import FreqDist
 from collections import Counter
 import matplotlib.pyplot as plt
+import pyttsx3 as p
 
 root = Tk()
 root.title('Bytesilog - SAPALARAN, MORAGA, RIOS')
 root.geometry("720x480")
 
+#Enter title
+def input_title():
+    title = input("Type the title of the file: ")
+    text_box.insert(END, title)
+
+#Open a file
 def open_file():
     try:
-        open_file = filedialog.askopenfilename()
+        open_file = filedialog.askopenfilename() #open a dialog box
         text_file = open(open_file, 'r')
-
         global read_file
         read_file = text_file.read()
 
-        text_box.insert(END, read_file)
+        #Insert the content of the file to the gui
+        text_box.insert(END, f'\n{read_file}')
         text_file.close()
 
         return text_file
@@ -25,6 +32,7 @@ def open_file():
         text_box.insert(END, "Invalid file")
         print("Invalid file")
 
+#count the words
 def count_words():
     global word_count
     word_count = Counter(word_tokenize(read_file))
@@ -43,17 +51,29 @@ def graph_word():
     fdist.plot(101, cumulative=False)
     plt.show()
 
-text_box = Text(root, width=100, height=20, font=("Times New Roman", 12))
-text_box.pack(pady=20)
+def speak():
+    engine = p.init()
+    rate = engine.getProperty('rate')
+    engine.setProperty('rate', 118)
+    engine.say(read_file)
+    engine.runAndWait()
+
+text_box = Text(root, width=100, height=20, font=("Helvetica", 12))
+text_box.pack(pady=10)
+
+enter_title_button = Button(root, text="Input", command=input_title)
+enter_title_button.pack(pady=10)
 
 open_button = Button(root, text="Attach", command=open_file)
-open_button.pack(pady=20)
+open_button.pack(pady=10)
 
 count_button = Button(root, text="Count", command=count_words)
-count_button.pack(pady=20)
+count_button.pack(pady=10)
 
 graph_button = Button(root, text="Graph", command=graph_word)
-graph_button.pack(pady=20)
+graph_button.pack(pady=10)
+
+read_button = Button(root, text="Read", command=speak)
+read_button.pack(pady=10)
 
 root.mainloop()
-
